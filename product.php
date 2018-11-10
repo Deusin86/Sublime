f9d57ca31a73ba9655db4d6347be1bc46211b1a8
Scorpion330 <44909837+Scorpion330@users.noreply.github.com>
2018-11-10 02:24:50 +0000

    Update product.php

--- product.php
+++ product.php
@@ -16,7 +16,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-
 <div class="super_container">
 
 	<!-- Header -->
@@ -100,6 +99,8 @@
 			</div>
 		</div>
 
+
+		<!--teste-->
 		<!-- Search Panel -->
 		<div class="search_panel trans_300">
 			<div class="container">
